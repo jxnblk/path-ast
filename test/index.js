9046a2917bc8781cae7828f3259e26dcbd7c6cee
Brent Jackson <jacksonblack@gmail.com>
2015-03-16 08:19:15 -0400

    Basic tests

--- test/index.js
+++ test/index.js
@@ -1,17 +1,37 @@
 
+var fs = require('fs');
 var pathAst = require('..');
+var assert = require('assert');
+
+// Fixtures
 var geomicons = require('geomicons-open/src/js/paths');
+var bookmark = pathAst.parse(geomicons.bookmark);
+var bookmarkMove = bookmark.commands[0].parameters;
+var camera = pathAst.parse(geomicons.camera);
+var cameraArc = camera.commands[10].parameters;
+var chat = pathAst.parse(geomicons.chat);
+var chatCurve = chat.commands[4].parameters;
 
-var array = [];
-Object.keys(geomicons).forEach(function(key) {
-  array.push(geomicons[key]);
+var chatString = pathAst.stringify(chat);
+
+describe('path-ast', function() {
+
+  it('should have two parameters for M', function() {
+    assert.equal( Object.keys(bookmarkMove).length, 2 );
+  });
+
+  it('should have seven parameters for A', function() {
+    assert.equal( Object.keys(cameraArc).length, 7 );
+  });
+
+  it('should have six parameters for C', function() {
+    assert.equal( Object.keys(chatCurve).length, 6 );
+  });
+
+  it('stringify should match input', function() {
+    assert.equal( geomicons.chat.trim(), chatString );
+  });
+
 });
 
-var results = [];
-array.forEach(function(path) {
-  results.push(pathAst.parse(path));
-});
-
-
-console.log(JSON.stringify(results, null, 2));
 
